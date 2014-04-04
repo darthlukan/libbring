d4694edfb3ca7013ad1b23222fb6b351f087c4fe
Brian Tomlinson <brian@smartm.no>
2014-04-04 23:59:06 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# LibBring v1.0
+
+> Author: Brian Tomlinson <brian.tomlinson@linux.com>
+
+
+## Description
+
+> LibBring is a Go wrapper for some of the most commonly used functions of the Bring API
+> located at http://developer.bring.com.
+
+
+## Usage
+
+> Example:
+
+```
+    import (
+        "fmt"
+        "github.com/darthlukan/libbring"
+    )
+
+    func main() {
+
+        data, err := libbring.Track("TESTPACKAGE-AT-PICKUPPOINT")
+
+        if err != nil {
+            panic(err)
+        }
+
+        fmt.Printf("Tracking info: %v", data)
+    }
+```
+
+> For more information on how to use this library, see the code in ```libbring/libbring.go```, the
+> functions in that file are very much self-explanatory.  [Bring API Documentation](http://developer.bring.com/index.html).
+
+
+## License
+
+> The Go code in this repo falls under GPLv2, see the LICENSE file.  The Bring API itself falls under separate
+> licensing conditions but is provided free of charge.  For more information, [visit their developer site.](http://developer.bring.com/index.html)
---
+++ libbring.go
@@ -0,0 +1,96 @@
+/*
+	LibBring
+
+	Description: A Go wrapper around some of the most commonly used functions of Bring.no's API.
+
+
+	Copyright (C) 2014  Brian C. Tomlinson
+
+	Contact: brian.tomlinson@linux.com
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License along
+	with this program; if not, write to the Free Software Foundation, Inc.,
+	51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+package libbring
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+var (
+	version         string = "1.0"
+	baseTrackingUrl string = "http://sporing.bring.no/sporing.json?q="
+	basePickUpUrl   string = "http://fraktguide.bring.no/fraktguide/api/pickuppoint"
+)
+
+// request takes a url as an argument and returns the JSON data
+// relevant to the request or an error if the request is not successful.
+func request(url string) (map[string]interface{}, error) {
+
+	response, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer response.Body.Close()
+
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := json.Unmarshal(body, &data); err != nil {
+		return nil, err
+	}
+
+	return data, nil
+}
+
+// Track takes a trackingNumber string as an argument, sets up the request
+// url, and then returns the JSON data of the request on success.
+func Track(trackingNumber string) (map[string]interface{}, error) {
+
+	url := fmt.Sprintf("%v%v", baseUrl, trackingNumber)
+	data, err := request(url)
+	return data, err
+}
+
+// PickByPostalCode takes a postalCode as an argument, sets up the request
+// url, and then returns the JSON data of the request on success.
+func PickByPostalCode(postalCode string) (map[string]interface{}, error) {
+
+	url := fmt.Sprintf("%v/postalcode/%v.json")
+	data, err := request(url)
+	return data, err
+}
+
+// PickByLocation takes the latitude and longitude as arguments, sets up the request
+// url, and then returns the JSON data of the request on success.
+func PickByLocation(lat, lon float64) (map[string]interface{}, error) {
+
+	url := fmt.Sprintf("%v/location/%v/%v.json", basePickUpUrl, lat, lon)
+	data, err := request(url)
+	return data, err
+}
+
+// PickById takes a pickuplocation id as an argument, sets up the request
+// url, and then returns the JSON data of the request on success.
+func PickById(id string) (map[string]interface{}, error) {
+
+	url := fmt.Sprintf("%v/id/%v.json", basePickUpUrl, id)
+	data, err := request(url)
+	return data, err
+}
